af989f2d61460e2115e819da8f40ccb2bd24d4cd
Vaibhav Sutar <apple@Apples-Air.lan>
2023-12-14 22:38:57 +0530

    HomePage

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import HomeScreen from './screens/HomeScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack'//
+import {createStackNavigator} from '@react-navigation/stack'
+import MapScreen from './screens/MapScreen';
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store ={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+    <Stack.Screen name='HomeScreen' component={HomeScreen} options={{headerShown: false}} />
+    <Stack.Screen name='MapScreen' component={MapScreen} options={{headerShown: false}} />
+
+        </Stack.Navigator>
+      <SafeAreaProvider>
+      {/* <HomeScreen/> */}
+      </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
